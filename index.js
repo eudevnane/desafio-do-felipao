07b2a09adbb2ed9ba1940cd648ac17946d963c81
eudevnane <nanewjmorais@icloud.com>
2023-09-21 13:00:54 -0300

    Revisão do código

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-let noome = "Capitão lee"
+let nickname = "Capitão lee"
 let xp = 2001 
 
-    if (xp >= 2001){
-        console.log("O herói de nome " + noome + " está no nível prata") 
+    if (xp >= 1000){
+       console.log("O herói de nome " + nickname + " está no nível ferro")  
     }
-
+     else if (xp >= 2001){
+       console.log("O herói de nome " + nickname + " está no nível bronze")  
+    }
+    else if (xp >= 5000){
+       console.log("O herói de nome " + nickname + " está no nível prata")  
+    }
+    else if (xp >= 7000){
+       console.log("O herói de nome " + nickname + " está no nível ouro")  
+    }
+    else (xp >= 8000) ; {
+       console.log("O herói de nome " + nickname + " está no nível platina") 
+    }
+    if (xp >= 9000){
+       console.log("O herói de nome " + nickname + " está no nível ascendente")  
+    }
+    else (xp >= 10000) ; {
+       console.log("O herói de nome " + nickname + " está no nível imortal")  
+    }
+    if (xp >= 20000){
+       console.log("O herói de nome " + nickname + " está no nível radiante")  
+    }
         
         
     
